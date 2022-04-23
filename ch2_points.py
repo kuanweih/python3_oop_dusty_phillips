28f6fec7fd0e9f8a0e967fe36dfd6815a4b60bfa
kuanweih@andrew.cmu.edu <{ID}+{username}@users.noreply.github.com>
2022-04-23 17:58:45 -0400

    add move method

--- ch2_points.py
+++ ch2_points.py
@@ -2,14 +2,15 @@ import math
 
 
 class Point:
-    """ Represents a point in 2D geometric coordinates.
+    """ Represent a point in 2D geometric coordinates.
     """    
     
     def __init__(self, x=0, y=0):
-        """ Initializes the position of a new point at (x, y).
+        """ Initialize the position of a new point at (x, y).
         """
+        self.move(x, y)
+        
+    def move(self, x, y):
+        """ Move the point to (x, y). """
         self.x = x
         self.y = y
-        
-    def move(self):
-        pass
