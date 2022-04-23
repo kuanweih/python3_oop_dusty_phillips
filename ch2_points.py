40a173f46f06bdb3370cbd2dd4d4d1ca9e5b7eff
kuanweih@andrew.cmu.edu <kuanweih@andrew.cmu.edu>
2022-04-23 17:46:02 -0400

    init

---
+++ ch2_points.py
@@ -0,0 +1,14 @@
+import math
+
+
+class Point:
+    """ Represents a point in 2D geometric coordinates.
+    """    
+    
+    def __init__(self, x=0, y=0):
+        """ Initializes the position of a new point at (x, y).
+        """
+        self.x = x
+        self.y = y
+        
+    
